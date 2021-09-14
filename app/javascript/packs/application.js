b8a79726074fd5964647295bd4ce68ba57bb2971
shobhna1 <shobhnasinha@yahoo.com>
2021-09-14 12:55:33 -0700

    add navigation, dropdown, controllers, routes and views

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,14 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery"
+import '@doabit/semantic-ui-sass'
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(document).on('turbolinks:load', function() {
+    $('.ui.dropdown').dropdown();
+})
